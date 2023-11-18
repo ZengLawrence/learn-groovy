ca3d70b18b51dcefb1849ede135931615bdb21df
Lawrence Zeng <lawrence.zeng@egmail.com>
2023-11-18 16:53:04 -0500

    simplify test

--- app/src/test/groovy/cf/parser/ServicesTest.groovy
+++ app/src/test/groovy/cf/parser/ServicesTest.groovy
@@ -6,20 +6,8 @@ import static Services.*
 class ServicesTest extends Specification {
 
     def "Line starts with 'name' is a header"() {
-
-        when:
-        def line = "name "
-
-        then:
-        isHeader(line)
-    }
-
-    def "Line does not start with 'name' is not a header"() {
-
-        when:
-        def line = "xxx "
-
-        then:
-        !isHeader(line)
+        expect:
+        isHeader("name")
+        !isHeader("xxx")
     }
 }
