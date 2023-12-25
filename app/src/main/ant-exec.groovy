f997b4ea326c85677492417884773c5053777d7a
Lawrence Zeng <lawrence.zeng@egmail.com>
2023-12-25 14:21:55 -0500

    include stack overflow answer

--- app/src/main/ant-exec.groovy
+++ app/src/main/ant-exec.groovy
@@ -19,7 +19,10 @@ ant.exec(executable: 'bash', resultproperty: 'my.cmd.exitCode') {
 }
 ant.echo('Exit code is ${my.cmd.exitCode}')
 // getting exit code from project property
-def cmdExitCode = ant.project.getProperty('my.cmd.exitCode')
+def cmdExitCode = ant.project.properties['my.cmd.exitCode']
 println "Exit code: ${cmdExitCode}"
 
 println "Project properteis: ${ant.project.properties}"
+
+// see https://stackoverflow.com/questions/77711984/groovy-antbuilder-how-to-get-process-exit-code-programmatically-from-ant-task-e/77713999#77713999
+assert ant.project == ant.antProject
