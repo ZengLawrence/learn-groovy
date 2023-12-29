e47eea3bab95bb222be4c36a53833b91b1e3f4a5
Lawrence Zeng <lawrence.zeng@egmail.com>
2023-12-29 15:16:16 -0500

    inject methods through script class

--- app/src/main/groovy/v02/dsl.groovy
+++ app/src/main/groovy/v02/dsl.groovy
@@ -1,16 +1,20 @@
 package v02
 
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ImportCustomizer
+import v02.integration.RobotBaseScript
 import v02.model.*
 
-def binding = new Binding(
-        robot: new Robot(),
-        *: Direction.values().collectEntries { [(it.name()): it] }
-)
+def binding = new Binding(robot: new Robot())
 
-def shell = new GroovyShell(this.class.classLoader, binding)
+def importCustomizer = new ImportCustomizer()
+importCustomizer.addStaticStars Direction.name
+
+def config = new CompilerConfiguration()
+config.addCompilationCustomizers importCustomizer
+config.scriptBaseClass = RobotBaseScript.name
+
+def shell = new GroovyShell(this.class.classLoader, binding, config)
 shell.evaluate '''
-@BaseScript(v02.integration.RobotBaseScript)
-import groovy.transform.BaseScript
-
 move left
 '''
